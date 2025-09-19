924e31f313016bcca6a3fa7016e79dacd9d42c5a
pardontingini <r166656a@dtudents.msu.ac.zw>
2025-09-19 12:12:06 +0200

    Updated ORM

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Collection, Customer, Product, Order, OrderItem, Address, Cart, CartItem
+from django.db.models import Q, F, Func, Value, Count
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # Get products that have been ordered, ordered by product title
+    queryset = Product.objects.only('id', 'title')
+    return render(request, 'hello.html', {'name': 'Pardon', 'products': list(queryset)})
